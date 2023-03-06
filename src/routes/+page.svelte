36d68e0d75bae6b0b96d29f482f0711d8c6e367b
Nintron <nintron@sent.com>
2023-03-05 23:58:56 -0500

    Add title & meta description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,13 @@
 	import InfoCards from "$lib/components/InfoCards.svelte";
 </script>
 
+<svelte:head>
+	<title>Stelo Finance</title>
+	<meta
+		name="description"
+		content="A finance platform for the game BitCraft. Free for any to use or even build apps on through our API."
+	/>
+</svelte:head>
+
 <Hero />
 <InfoCards />
